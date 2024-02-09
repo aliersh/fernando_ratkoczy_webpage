7a10db144af8058e9a1ca0047c644bb9e567b81a
aliersh <ariel.diaz.borquez@icloud.com>
2024-02-09 16:07:31 -0500

    Adjust navbar behavior

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,31 @@ import "bootstrap";
 if (module.hot) {
     module.hot.accept();
 }
+
+// Adjustment scroll for fixed top navbar
+
+const navbarScroll = (targetId) => {
+    setTimeout(() => {
+        const navbarHeight = document.querySelector(".navbar").offsetHeight;
+        const target = document.querySelector(targetId);
+
+        window.scroll({
+            top: target.offsetTop - navbarHeight,
+            behavior: "smooth",
+        });
+    }, 320);
+};
+
+document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+        e.preventDefault();
+        navbarScroll(anchor.getAttribute("href"));
+    });
+});
+
+window.addEventListener("resize", () => {
+    const currentSection = window.location.hash;
+    if (currentSection) {
+        navbarScroll(currentSection);
+    }
+});
