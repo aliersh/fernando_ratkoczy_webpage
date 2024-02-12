5dc74ad7254b80d5e09621b1b4daa1ec7d5f2b45
aliersh <ariel.diaz.borquez@icloud.com>
2024-02-12 14:59:12 -0500

    Add logic to click outside the navbar to collapse

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,7 +16,7 @@ const navbarScroll = (targetId) => {
             top: target.offsetTop - navbarHeight,
             behavior: "smooth",
         });
-    }, 325);
+    }, 325); // Time out to wait until calculate the height of the element
 };
 
 document.querySelectorAll("a[href^='#']").forEach((anchor) => {
@@ -33,7 +33,7 @@ window.addEventListener("resize", () => {
     }
 });
 
-// Disable scrollspy in < md size screens
+// Scrollspy function (disabled in < md size screens)
 
 const scrollSpyElm = document.querySelector('[data-bs-spy="scroll"]');
 const breakpoint = 768;
@@ -52,3 +52,19 @@ const updateScrollSpy = () => {
 
 window.addEventListener("resize", updateScrollSpy);
 document.addEventListener("DOMContentLoaded", updateScrollSpy);
+
+// Click on any part to collapse the navbar toggle button
+
+document.addEventListener("click", (e) => {
+    let isClickInsideNavbar = document
+        .getElementById("navbar")
+        .contains(e.target);
+    let navbarToggler = document.querySelector(".navbar-toggler");
+
+    if (
+        !isClickInsideNavbar &&
+        navbarToggler.classList.contains("collapsed") === false
+    ) {
+        navbarToggler.click();
+    }
+});
